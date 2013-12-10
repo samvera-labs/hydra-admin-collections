9689bc9209f2a58c980750cda803bf1f4e5a2690
Brian Maddy <brian@brianmaddy.com>
2013-12-10 11:15:39 -0600

    added some new attributes

--- app/models/concerns/hydra/admin/collections/metadata.rb
+++ app/models/concerns/hydra/admin/collections/metadata.rb
@@ -4,9 +4,15 @@ module Hydra::Admin::Collections::Metadata
     has_metadata 'descMetadata', type: ActiveFedora::SimpleDatastream do |sds|
       sds.field :name, :string
       sds.field :description, :string
+      sds.field :publisher, :string
+      sds.field :type, :string
+      sds.field :language, :string
+      sds.field :coverage, :string
+      sds.field :creators, :string
     end
 
-    has_attributes :description, :name, datastream: :descMetadata, multiple: false
+    has_attributes :description, :name, :publisher, :type, :language, :coverage, datastream: :descMetadata, multiple: false
+    has_attributes :creators, datastream: :descMetadata, multiple: true
   end
 end
 
